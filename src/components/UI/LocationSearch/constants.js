7207cd833a88f75d7020271c1e28d651b53fdf95
VPresich <valya.alexandrova.presich@gmail.com>
2024-09-23 03:58:55 +0200

    deleted unusable component

--- src/components/UI/LocationSearch/constants.js
+++
@@ -1,2 +0,0 @@
-export const ERR_REQUIRED = "String is required";
-export const ERR_SHORT = "String is too short ( must be > 2)";
--- src/components/UI/LocationSearch/feedbackScema.js
+++
@@ -1,5 +0,0 @@
-import * as Yup from "yup";
-
-export const feedbackSchema = Yup.object().shape({
-  topic: Yup.string(),
-});
